4deb4b56e2b36f0b6c343ab49d6f76b0908b1f70
J.B. Langston <jb-langston@austin.rr.com>
2014-01-14 16:59:14 -0600

    Fix exception parsing and add examples

--- cassandra.py
+++ cassandra.py
@@ -18,9 +18,9 @@ class regex:
             if match:
                 fields = match.groupdict()
                 action(log, fields, extra_fields)
-                return True
+                return fields
             else:
-                return False
+                return None
         return rule
 
     
@@ -49,13 +49,16 @@ class SystemLog:
     def __init__(self, files=None):
         self.lines = []
         self.sessions = [{}]
-        self.threads = {}
+        self.field_stack = collections.defaultdict(list)
+        self.unrecognized = []
         fi = fileinput.FileInput(files)
         for line in fi:
             for rule in self.line_rules:
-                line_fields = rule(self, line, extra_fields=dict(log_file=fi.filename(), log_lineno=fi.filelineno()))
+                line_fields = rule(self, line.strip(), extra_fields=dict(log_file=fi.filename(), log_lineno=fi.filelineno()))
                 if line_fields is not None:
                     break
+            else:
+            	print line
                     
     def update_session(self, key, fields):
         if key not in self.sessions[-1]:
@@ -67,57 +70,64 @@ class SystemLog:
             self.sessions[-1][key] = []
         self.sessions[-1][key].append(fields)
         
-    def push_thread(self, thread_name, thread_id, fields):
-        self.threads[(thread_name, thread_id)] = fields
+    def push_fields(self, key, fields):
+        self.field_stack[key].append(fields)
         
-    def pop_thread(self, thread_name, thread_id):
-        if (thread_name, thread_id) in self.threads:
-            fields = self.threads[(thread_name, thread_id)]
-            del self.threads[(thread_name, thread_id)]
-            return fields
-        else:
-            return {}
+    def pop_fields(self, key):
+		return {} if self.field_stack[key] else self.field_stack[key].pop()
+
 
     @group(line_rules)
-    @regex(r'(?P<level>.{5}) \[(?P<thread_name>[A-Za-z]*):?(?P<thread_id>[0-9]*)\] (?P<date>.{10} .{12}) (?P<source_file>[^ ]*) \(line (?P<source_lineno>[0-9]*)\) (?P<message>.*)')
+    @regex(r'(?P<level>[A-Z]{4,5}) \[(?P<thread>[^\]]*)\] (?P<date>.{10} .{12}) (?P<source_file>[^ ]*) \(line (?P<source_lineno>[0-9]*)\) (?P<message>.*)')
     def message_line(self, line_fields, extra_fields):
         'Parse main message line'
         line_fields['level'] = line_fields['level'].strip()
         line_fields['date'] = datetime.datetime.strptime(line_fields['date'], '%Y-%m-%d %H:%M:%S,%f')
-        line_fields['thread_id'] = None if line_fields['thread_id'] == '' else int(line_fields['thread_id'])
         if extra_fields is not None:
             line_fields.update(extra_fields)
         for rule in self.message_rules[line_fields['source_file'][:-5]]:
             if rule(self, line_fields['message'], line_fields):
                 break
+        else:
+        	self.unrecognized.append(line_fields)
+        if line_fields['level'] == 'ERROR':
+        	self.append_session('errors', line_fields)
+        elif line_fields['level'] == 'WARN':
+        	self.append_session('warnings', line_fields)
         self.lines.append(line_fields)
 
     @group(line_rules)
-    @regex(r'(?P<exception>[^:]*): (?P<exception_message>.*)')
+    @regex(r'^(?!Caused by:)(?P<exception>[^:]*): (?P<exception_message>.*)')
     def exception_line(self, line_fields, extra_fields):
         'Parse exception line and update previous message line'
         self.lines[-1].update(line_fields)
-        
+
     @group(line_rules)
-    @regex(r'at (?P<method>[^(]*)\((?P<source_file>[^:]*):(?P<source_lineno>[0-9]*)\)')
+    @regex(r'^Caused by: (?P<exception>[^:]*): (?P<exception_message>.*)')
+    def caused_by_line(self, line_fields, extra_fields):
+        'Parse nested exceptions'
+        if 'caused_by' not in self.lines[-1]:
+            self.lines[-1]['caused_by'] = []
+        self.lines[-1]['caused_by'].append(line_fields)
+
+    @group(line_rules)
+    @regex(r'at (?P<method>[^(]*)\((?P<source_file>[^:)]*):?(?P<source_lineno>[0-9]*)\)')
     def trace_line(self, line_fields, extra_fields):
         'Parse stack trace line and append to previous message line'
-        line_fields['source_lineno'] = int(line_fields['source_lineno'])
+        line_fields['source_lineno'] = None if line_fields['source_lineno'] == '' else int(line_fields['source_lineno'])
         components = line_fields['method'].split('.')
         line_fields['package'] = '.'.join(components[:-3])
         line_fields['class'] = components[-2]
         line_fields['method'] = components[-1]
-        if 'caused_by' in self.line[-1]:
-            self.lines[-1]['caused_by'][-1].get('stack_trace', []).append(line_fields)
+        if 'caused_by' in self.lines[-1]:
+            if 'stack_trace' not in self.lines[-1]['caused_by'][-1]:
+        	    self.lines[-1]['caused_by'][-1]['stack_trace'] = []
+            self.lines[-1]['caused_by'][-1]['stack_trace'].append(line_fields)
         else:
-            self.lines[-1].get('stack_trace', []).append(line_fields)
+            if 'stack_trace' not in self.lines[-1]:
+                self.lines[-1]['stack_trace'] = []
+            self.lines[-1]['stack_trace'].append(line_fields)
         
-    @group(line_rules)
-    @regex(r'Caused by: (?P<exception>[^:]*): (?P<exception_message>.*)')
-    def caused_by_line(self, line_fields, extra_fields):
-        'Parse nested exceptions'
-        self.lines[-1].get('caused_by', []).append(line_fields)
-
     @group(message_rules['CassandraDaemon'])
     @regex(r'Logging initialized')
     def new_session(self, message_fields, line_fields):
@@ -143,7 +153,7 @@ class SystemLog:
         message_fields['classpath'] = message_fields['classpath'].split(':')
         self.update_session('environment', message_fields)
         
-    @group(message_rules['CassandraDaemon'], message_rules['StorageService'])
+    @group(message_rules['DseDaemon'], message_rules['StorageService'])
     @regex(r'(?P<component>[A-Za-z ]*) versions?: (?P<version>.*)')
     def component_version(self, message_fields, line_fields):
         self.append_session('versions', message_fields)
@@ -167,14 +177,14 @@ class SystemLog:
     def begin_flush(self, message_fields, line_fields):
         message_fields['begin_date'] = line_fields['date']
         convert(message_fields, ('address', 'serialized_bytes', 'live_bytes', 'ops'), int)
-        self.push_thread(line_fields['thread_name'], line_fields['thread_id'], message_fields)
+        self.push_fields(line_fields['thread'], message_fields)
     
     @group(message_rules['Memtable'])
     @regex(r'Completed flushing (?P<filename>[^ ]*) \((?P<file_size>[0-9]*) bytes\) for commitlog position ReplayPosition\(segmentId=(?P<segment_id>[0-9]*), position=(?P<position>[0-9]*)\)')
     def end_flush(self, message_fields, line_fields):
         message_fields['end_date'] = line_fields['date']
         convert(message_fields, ('file_size', 'segment_id', 'position'), int)
-        message_fields.update(self.pop_thread(line_fields['thread_name'], line_fields['thread_id']))
+        message_fields.update(self.pop_fields(line_fields['thread']))
         self.append_session('flushes', message_fields)
 			
     @group(message_rules['CompactionTask'])
@@ -182,7 +192,7 @@ class SystemLog:
     def begin_compaction(self, message_fields, line_fields):
         message_fields['begin_date'] = line_fields['date']
         message_fields['input_sstables'] = [sstable[20:-2] for sstable in message_fields['input_sstables'].split(', ')]
-        self.push_thread(line_fields['thread_name'], line_fields['thread_id'], message_fields)
+        self.push_fields(line_fields['thread'], message_fields)
 
     @group(message_rules['CompactionTask'])
     @regex(r'Compacted (?P<sstable_count>[0-9]*) sstables to \[(?P<output_sstable>[^,]*),\].  (?P<input_bytes>[0-9,]*) bytes to (?P<output_bytes>[0-9,]*) \(~(?P<percent_of_original>[0-9]*)% of original\) in (?P<duration>[0-9,]*)ms = (?P<rate>[0-9.]*)MB/s.  (?P<total_rows>[0-9,]*) total rows, (?P<unique_rows>[0-9,]*) unique.  Row merge counts were \{(?P<row_merge_counts>[^}]*)\}')
@@ -192,5 +202,5 @@ class SystemLog:
         convert(message_fields, 
                 ('sstable_count', 'input_bytes', 'output_bytes', 'percent_of_original', 'duration', 'total_rows', 'unique_rows'), 
                 lambda value: int(value.replace(',', '')))
-        message_fields.update(self.pop_thread(line_fields['thread_name'], line_fields['thread_id']))
+        message_fields.update(self.pop_fields(line_fields['thread']))
         self.append_session('compactions', message_fields)
