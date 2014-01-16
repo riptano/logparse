9b8fdd7b2167ad4fab32bdc1e90a00124987e8e0
J.B. Langston <jb-langston@austin.rr.com>
2014-01-16 11:52:46 -0600

    Add rules for repair sessions

--- cassandra.py
+++ cassandra.py
@@ -11,7 +11,7 @@ class regex:
             self.regex = re.compile(regex)
         else:
             self.regex = regex
-    
+
     def __call__(self, action):
         def rule(log, input, extra_fields=None):
             match = self.regex.match(input)
@@ -23,12 +23,12 @@ class regex:
                 return None
         return rule
 
-    
+
 class group:
     'Decorator registers a rule in the specified list.'
     def __init__(self, *args):
         self.rule_lists = args
-    
+
     def __call__(self, rule):
         for rule_list in self.rule_lists:
             rule_list.append(rule)
@@ -40,16 +40,16 @@ def convert(dictionary, field_list, func):
     for field in field_list:
         dictionary[field] = func(dictionary[field])
 
-    
+
 class SystemLog:
     'Cassandra system.log parser'
     line_rules = []
     message_rules = collections.defaultdict(list)
-    
+
     def __init__(self, files=None):
         self.lines = []
         self.sessions = [{}]
-        self.field_stack = collections.defaultdict(list)
+        self.saved_fields = collections.defaultdict(dict)
         self.unknown_lines = []
         self.unknown_messages = []
         fi = fileinput.FileInput(files)
@@ -60,23 +60,16 @@ class SystemLog:
                     break
             else:
                 self.unknown_lines.append(line)
-                    
+
     def update_session(self, key, fields):
         if key not in self.sessions[-1]:
             self.sessions[-1][key] = {}
         self.sessions[-1][key].update(fields)
-        
+
     def append_session(self, key, fields):
         if key not in self.sessions[-1]:
             self.sessions[-1][key] = []
         self.sessions[-1][key].append(fields)
-        
-    def push_fields(self, key, fields):
-        self.field_stack[key].append(fields)
-        
-    def pop_fields(self, key):
-		return self.field_stack[key].pop() if self.field_stack[key] else {}
-
 
     @group(line_rules)
     @regex(r'(?P<level>[A-Z]{4,5}) \[(?P<thread>[^\]]*)\] (?P<date>.{10} .{12}) (?P<source_file>[^ ]*) \(line (?P<source_lineno>[0-9]*)\) (?P<message>.*)')
@@ -92,9 +85,9 @@ class SystemLog:
         else:
             self.unknown_messages.append(line_fields)
         if line_fields['level'] == 'ERROR':
-        	self.append_session('errors', line_fields)
+            self.append_session('errors', line_fields)
         elif line_fields['level'] == 'WARN':
-        	self.append_session('warnings', line_fields)
+            self.append_session('warnings', line_fields)
         self.lines.append(line_fields)
 
     @group(line_rules)
@@ -122,13 +115,13 @@ class SystemLog:
         line_fields['method'] = components[-1]
         if 'caused_by' in self.lines[-1]:
             if 'stack_trace' not in self.lines[-1]['caused_by'][-1]:
-        	    self.lines[-1]['caused_by'][-1]['stack_trace'] = []
+                self.lines[-1]['caused_by'][-1]['stack_trace'] = []
             self.lines[-1]['caused_by'][-1]['stack_trace'].append(line_fields)
         else:
             if 'stack_trace' not in self.lines[-1]:
                 self.lines[-1]['stack_trace'] = []
             self.lines[-1]['stack_trace'].append(line_fields)
-        
+
     @group(message_rules['CassandraDaemon'])
     @regex(r'Logging initialized')
     def new_session(self, message_fields, line_fields):
@@ -136,36 +129,36 @@ class SystemLog:
         if self.sessions != [{}]:
             self.sessions.append({})
         self.sessions[-1]['start_date'] = line_fields['date']
-    
+
     @group(message_rules['CassandraDaemon'])
     @regex(r'JVM vendor/version: (?P<jvm>.*)')
     def jvm_vendor(self, message_fields, line_fields):
         self.update_session('environment', message_fields)
-    
+
     @group(message_rules['CassandraDaemon'])
     @regex(r'Heap size: (?P<heap_used>[0-9]*)/(?P<total_heap>[0-9]*)')
     def heap_size(self, message_fields, line_fields):
         convert(message_fields, ('heap_used', 'total_heap'), int)
         self.update_session('environment', message_fields)
-    
+
     @group(message_rules['CassandraDaemon'])
     @regex(r'Classpath: (?P<classpath>.*)')
     def classpath(self, message_fields, line_fields):
         message_fields['classpath'] = message_fields['classpath'].split(':')
         self.update_session('environment', message_fields)
-        
+
     @group(message_rules['DseDaemon'], message_rules['StorageService'])
     @regex(r'(?P<component>[A-Za-z ]*) versions?: (?P<version>.*)')
     def component_version(self, message_fields, line_fields):
         self.append_session('versions', message_fields)
-        
+
     @group(message_rules['GCInspector'])
     @regex(r'Heap is (?P<percent_full>[0-9.]*) full.*')
     def heap_full(self, message_fields, line_fields):
         message_fields['date'] = line_fields['date']
         message_fields['percent_full'] = float(message_fields['percent_full'])
         self.append_session('heap_warnings', message_fields)
-	
+
     @group(message_rules['GCInspector'])
     @regex(r'GC for (?P<type>[A-Za-z]*): (?P<duration>[0-9]*) ms for (?P<collections>[0-9]*) collections, (?P<used>[0-9]*) used; max is (?P<max>[0-9]*)')
     def garbage_collection(self, message_fields, line_fields):
@@ -178,32 +171,34 @@ class SystemLog:
     def begin_flush(self, message_fields, line_fields):
         message_fields['begin_date'] = line_fields['date']
         convert(message_fields, ('hash_code', 'serialized_bytes', 'live_bytes', 'ops'), int)
-        self.push_fields(line_fields['thread'], message_fields)
-    
+        self.saved_fields[line_fields['thread']] = message_fields
+
     @group(message_rules['Memtable'])
     @regex(r'Completed flushing (?P<filename>[^ ]*) \((?P<file_size>[0-9]*) bytes\) for commitlog position ReplayPosition\(segmentId=(?P<segment_id>[0-9]*), position=(?P<position>[0-9]*)\)')
     def end_flush(self, message_fields, line_fields):
         message_fields['end_date'] = line_fields['date']
         convert(message_fields, ('file_size', 'segment_id', 'position'), int)
-        message_fields.update(self.pop_fields(line_fields['thread']))
+        message_fields.update(self.saved_fields[line_fields['thread']])
+        del self.saved_fields[line_fields['thread']]
         self.append_session('flushes', message_fields)
-			
+
     @group(message_rules['CompactionTask'])
     @regex(r'Compacting \[(?P<input_sstables>[^\]]*)\]')
     def begin_compaction(self, message_fields, line_fields):
         message_fields['begin_date'] = line_fields['date']
         message_fields['input_sstables'] = [sstable[20:-2] for sstable in message_fields['input_sstables'].split(', ')]
-        self.push_fields(line_fields['thread'], message_fields)
+        self.saved_fields[line_fields['thread']] = message_fields
 
     @group(message_rules['CompactionTask'])
     @regex(r'Compacted (?P<sstable_count>[0-9]*) sstables to \[(?P<output_sstable>[^,]*),\].  (?P<input_bytes>[0-9,]*) bytes to (?P<output_bytes>[0-9,]*) \(~(?P<percent_of_original>[0-9]*)% of original\) in (?P<duration>[0-9,]*)ms = (?P<rate>[0-9.]*)MB/s.  (?P<total_rows>[0-9,]*) total rows, (?P<unique_rows>[0-9,]*) unique.  Row merge counts were \{(?P<row_merge_counts>[^}]*)\}')
     def end_compaction(self, message_fields, line_fields):
         message_fields['end_date'] = line_fields['date']
         message_fields['rate'] = float(message_fields['rate'])
-        convert(message_fields, 
-                ('sstable_count', 'input_bytes', 'output_bytes', 'percent_of_original', 'duration', 'total_rows', 'unique_rows'), 
+        convert(message_fields,
+                ('sstable_count', 'input_bytes', 'output_bytes', 'percent_of_original', 'duration', 'total_rows', 'unique_rows'),
                 lambda value: int(value.replace(',', '')))
-        message_fields.update(self.pop_fields(line_fields['thread']))
+        message_fields.update(self.saved_fields[line_fields['thread']])
+        del self.saved_fields[line_fields['thread']]
         self.append_session('compactions', message_fields)
 
     @group(message_rules['CompactionController'])
@@ -211,8 +206,74 @@ class SystemLog:
     def incremental_compaction(self, message_fields, line_fields):
         message_fields['row_size'] = int(message_fields['row_size'])
         message_fields['date'] = line_fields['date']
-        compaction = self.pop_fields(line_fields['thread'])
+        compaction = self.saved_fields[line_fields['thread']]
         if 'incremental_rows' not in compaction:
             compaction['incremental_rows'] = []
         compaction['incremental_rows'].append(message_fields)
-        self.push_fields(line_fields['thread'], compaction)
+
+    @group(message_rules['AntiEntropyService'])
+    @regex(r'\[repair #(?P<session_id>[^\]]*)\] new session: will sync (?P<nodes>[^o]*) on range \((?P<range_begin>[^,]*),(?P<range_end>[^\]]*)\] for (?P<keyspace>[^.]*)\.\[(?P<column_families>[^\]]*)\]')
+    def begin_repair(self, message_fields, line_fields):
+        message_fields['start_date'] = line_fields['date']
+        message_fields['nodes'] = message_fields['nodes'].split(', ')
+        message_fields['column_families'] = message_fields['column_families'].split(', ')
+        self.saved_fields[message_fields['session_id']] = message_fields
+
+    @group(message_rules['AntiEntropyService'])
+    @regex(r'\[repair #(?P<session_id>[^\]]*)\] requesting merkle trees for (?P<column_family>[^ ]*) \(to \[(?P<nodes>[^\]]*)\]\)')
+    def merkle_requested(self, message_fields, line_fields):
+        merkle_requests = self.saved_fields[message_fields['session_id']].get('merkle_requests', {})
+        for node in message_fields['nodes'].split(', '):
+            merkle_requests[message_fields['column_family'], node] = {
+                'request_date': line_fields['date'],
+                'column_family': message_fields['column_family'],
+                'node': node
+            }
+        self.saved_fields[message_fields['session_id']]['merkle_requests'] = merkle_requests
+
+    @group(message_rules['AntiEntropyService'])
+    @regex(r'\[repair #(?P<session_id>[^\]]*)\] Received merkle tree for (?P<column_family>[^ ]*) from (?P<node>.*)')
+    def merkle_received(self, message_fields, line_fields):
+        merkle_requests = self.saved_fields[message_fields['session_id']].get('merkle_requests', {})
+        merkle_requests[message_fields['column_family'], message_fields['node']]['receive_date'] = line_fields['date']
+        self.saved_fields[message_fields['session_id']]['merkle_requests'] = merkle_requests
+
+    @group(message_rules['AntiEntropyService'])
+    @regex(r'\[repair #(?P<session_id>[^\]]*)\] Sending completed merkle tree to (?P<node>[^ ]*) for \((?P<keyspace>[^,]*),(?P<column_family>[^)]*)\)')
+    def merkle_sent(self, message_fields, line_fields):
+        message_fields['date'] = line_fields['date']
+        sent_merkles = self.saved_fields[message_fields['session_id']].get('sent_merkles', [])
+        sent_merkles.append(message_fields)
+        self.saved_fields[message_fields['session_id']]['sent_merkles'] = sent_merkles
+
+    @group(message_rules['AntiEntropyService'])
+    @regex(r'\[repair #(?P<session_id>[^\]]*)\] Endpoints (?P<node1>[^ ]*) and (?P<node2>[^ ]*) are consistent for (?P<column_family>.*)')
+    def endpoints_consistent(self, message_fields, line_fields):
+        message_fields['date'] = line_fields['date']
+        consistent_endpoints = self.saved_fields[message_fields['session_id']].get('consistent_endpoints', [])
+        consistent_endpoints.append(message_fields)
+        self.saved_fields[message_fields['session_id']]['consistent_endpoints'] = consistent_endpoints
+
+    @group(message_rules['AntiEntropyService'])
+    @regex(r'\[repair #(?P<session_id>[^\]]*)\] Endpoints (?P<node1>[^ ]*) and (?P<node2>[^ ]*) have (?P<ranges>[0-9]*) range\(s\) out of sync for (?P<column_family>.*)')
+    def endpoints_inconsistent(self, message_fields, line_fields):
+        message_fields['date'] = line_fields['date']
+        inconsistent_endpoints = self.saved_fields[message_fields['session_id']].get('inconsistent_endpoints', [])
+        inconsistent_endpoints.append(message_fields)
+        self.saved_fields[message_fields['session_id']]['inconsistent_endpoints'] = inconsistent_endpoints
+
+    @group(message_rules['AntiEntropyService'])
+    @regex(r'\[repair #(?P<session_id>[^\]]*)\] (?P<column_family>[^ ]*) is fully synced( \((?P<cfs_remaining>[0-9]*) remaining column family to sync for this session\))?')
+    def columnfamily_synced(self, message_fields, line_fields):
+        message_fields['date'] = line_fields['date']
+        synced_columnfamilies = self.saved_fields[message_fields['session_id']].get('synced_columnfamilies', [])
+        synced_columnfamilies.append(message_fields)
+        self.saved_fields[message_fields['session_id']]['synced_columnfamilies'] = synced_columnfamilies
+
+    @group(message_rules['AntiEntropyService'])
+    @regex(r'\[repair #(?P<session_id>[^\]]*)\] session completed successfully')
+    def end_repair(self, message_fields, line_fields):
+        repair_session = self.saved_fields[message_fields['session_id']]
+        repair_session['end_date'] = line_fields['date']
+        self.append_session('repair_sessions', repair_session)
+        del self.saved_fields[message_fields['session_id']]
