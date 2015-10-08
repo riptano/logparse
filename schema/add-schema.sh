9d30b16a36f8a9fdadeba15713af6045c03fba53
J.B. Langston <jblangston@datastax.com>
2015-10-08 11:22:10 -0400

    Switch to using dsetool create_core to create solr schema

--- schema/add-schema.sh
+++ schema/add-schema.sh
@@ -1,31 +1,4 @@
 #!/bin/sh
-
-# options parser sets the following variables:
-# HOST - server hostname
-# SCHEME  - (http|https)
-# CERT_FILE - client certificate file
-# AUTH_OPTS - Additional security options for curl
-cd `dirname $0`
-. ./set-solr-options.sh $*
-
-cqlsh -f ../systemlog.cql
-
-SOLRCONFIG_URL="$SCHEME://$HOST:8983/solr/resource/logparse.systemlog/solrconfig.xml"
-SOLRCONFIG=solrconfig.xml
-
-echo "Posting $SOLRCONFIG to $SOLRCONFIG_URL..."
-curl -s $AUTH_OPTS $CLIENT_CERT_FILE $CERT_FILE --data-binary @$SOLRCONFIG -H 'Content-type:text/xml; charset=utf-8' $SOLRCONFIG_URL
-echo "Posted $SOLRCONFIG to $SOLRCONFIG_URL"
-
-SCHEMA_URL="$SCHEME://$HOST:8983/solr/resource/logparse.systemlog/schema.xml"
-SCHEMA=schema.xml
-
-echo "Posting $SCHEMA to $SCHEMA_URL..."
-curl -s $AUTH_OPTS $CLIENT_CERT_FILE $CERT_FILE --data-binary @$SCHEMA -H 'Content-type:text/xml; charset=utf-8' $SCHEMA_URL
-echo "Posted $SCHEMA to $SCHEMA_URL"
-
-CREATE_URL="$SCHEME://$HOST:8983/solr/admin/cores?action=CREATE&name=logparse.systemlog"
-
-echo "Creating index..."
-curl -s $AUTH_OPTS $CLIENT_CERT_FILE $CERT_FILE  $CREATE_URL 
-echo "Created index."
+SCHEMA_PATH=`dirname $0`
+cqlsh -f $SCHEMA_PATH/systemlog.cql
+dsetool create_core logparse.systemlog schema=$SCHEMA_PATH/schema.xml solrconfig=$SCHEMA_PATH/solrconfig.xml
