940f0ef95d9e2d3f5e75164951c53b0ca692daf9
Brandon Williams <brandon@datastax.com>
2016-02-04 17:19:19 -0600

    reflect large partition size

--- clography.py
+++ clography.py
@@ -73,6 +73,8 @@ for event in systemlog.parse_log(log):
             data[ts][stage] += event['messages_dropped']
     elif event['event_type'] == 'begin_flush' and event['serialized_bytes'] != None:
         data[ts][stage] += (event['serialized_bytes'] / 1024**2 / scale)
+    elif event['event_type'] in ('incremental_compaction', 'large_partition'):
+        data[ts][stage] += event['partition_size'] / 1024**2 / scale
     else:
         data[ts][stage] += 1
 
